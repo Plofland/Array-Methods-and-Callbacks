1afebb0d11802781687c73a6ac4252cb585a1e2a
Plofland <ploflan@gmail.com>
2020-11-10 16:18:40 -0500

    Task 2 finished

--- index.js
+++ index.js
@@ -16,27 +16,51 @@ console.log('Task 1a: ', finals2014[0]["Home Team Name"])
 
 //(b) Away Team name for 2014 world cup final
 
+console.log('Task 1b: ', finals2014[0]["Away Team Name"])
+
 //(c) Home Team goals for 2014 world cup final
+
+console.log('Task 1c: ', finals2014[0]["Home Team Goals"])
 
 //(d) Away Team goals for 2014 world cup final
 
+console.log('Task 1d: ', finals2014[0]["Away Team Goals"])
+
 //(e) Winner of 2014 world cup final */
+
+console.log('Task 1e: ', finals2014[0]["Win conditions"])
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-   /* code here */
+// function getFinals(data) {
+//    return getFinals.filter(function(item){
+//        return fifaData.Stage === "Final";
+//    });
+// }
+// console.log(getFinals());
+
+function getFinals(data){
+    const finalTeams = data.filter(function(item){
+        return item.Stage === "Final";
+    });
+    return finalTeams;
 }
+// getFinals();
+// console.log('Task 2: ', finalTeams);
 
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(getFinals){
+    let finalYears = [],
+    finalTeams.forEach(function(item){
+        finalYears.push(fifaData.Year);
+    })
+    return finalYears;
 }
 
 
